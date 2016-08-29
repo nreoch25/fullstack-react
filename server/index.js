c1787f1bc52b89b465201e090a4349ee7199e153
Nigel Reoch <nigel.reoch@cbc.ca>
2016-08-28 20:03:31 -0400

    server side isomorphic updates

--- server/index.js
+++ server/index.js
@@ -11,9 +11,9 @@ import React from "react";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { renderToStaticMarkup } from "react-dom/server";
+import createLocation from 'history/lib/createLocation';
 import ReactRouter from "react-router";
 import { RouterContext, match } from "react-router";
-import * as hist from "history";
 import rootReducer from "../src/reducers";
 import reactRoutes from "../src/routes";
 import fetchServerSide from "./fetchServerSide";
@@ -28,7 +28,49 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use("/model.json", falcorExpress.dataSourceRoute(function(req, res) {
   return new Router(routes);
 }));
-app.use(express.static("dist"));
+let handleServerSideRender = (req, res) => {
+  let initMOCKstore = fetchServerSide();
+  // mocked for now // Create a Redux store instance
+  const store = createStore(rootReducer, initMOCKstore);
+  const location = createLocation(req.url);
+  match({ routes: reactRoutes, location }, (err, redirectLocation, renderProps) => {
+    console.log(renderProps);
+    if(redirectLocation) {
+      res.redirect(301, redirectLocation.pathname = redirectLocation.search);
+    } else if(err) {
+      console.log(err);
+      next(err); // res.send(500, err.message);
+    } else if(renderProps === null) {
+      res.status(404).send("Not Found");
+    } else {
+      let html = renderToStaticMarkup(
+        <Provider store={store}>
+          <RouterContext {...renderProps} />
+        </Provider>
+      );
+      const initialState = store.getState();
+      let fullHTML = renderFullPage(html, initialState);
+      res.send(fullHTML)
+    }
+  });
+};
+let renderFullPage = (html, initialState) => {
+  return `
+    <!doctype html>
+    <html>
+      <head>
+        <title>Publishing App Server Side Rendering</title>
+      </head>
+      <body>
+        <h1>Server side publishing app</h1>
+        <div id="publishingAppRoot">${html}</div>
+        <script>window.__INITIAL_STATE__ = ${JSON.stringify(initialState)}</script>
+        <script src="/static/app.js"></script>
+      </body>
+    </html> `
+};
+app.use(handleServerSideRender);
+app.use("/static", express.static("dist"));
 app.server.listen(process.env.PORT || 3000);
 console.log(`Started on port ${app.server.address().port}`);
 export default app;
